071c0aebe707fec01b11b66b68be62c35a97b7e6
Burak Ucun <139056365+burakutf@users.noreply.github.com>
2023-11-24 19:51:29 +0300

    admin_site_register_tenant

--- qlab/apps/tenant/admin.py
+++ qlab/apps/tenant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from qlab.apps.tenant.models import Domain, Organization
+
 # Register your models here.
+admin.site.register(Organization)
+admin.site.register(Domain)
